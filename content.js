2e3b6c1843340c7694125bb1fa3bd8333e6c6659
Alvee Mir <93854226+bardicbyte@users.noreply.github.com>
2023-11-03 01:32:15 +0600

    Update content.js

--- content.js
+++ content.js
@@ -20,3 +20,42 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         }
     }
 });
+
+
+// Function to call OpenAI API and get the summary
+function getSummary(text) {
+    const apiKey = 'YOUR_OPENAI_API_KEY'; // Replace with your OpenAI API key
+    const data = {
+        prompt: text,
+        max_tokens: 100, // Adjust based on desired summary length
+        temperature: 0.7
+    };
+
+    fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const summary = data.choices[0].text.trim();
+        // Display the summary (replace the alert with a proper UI in the next steps)
+        alert(summary);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "summarize") {
+        const selectedText = request.text;
+        if (selectedText) {
+            getSummary(selectedText);
+        }
+    }
+});
